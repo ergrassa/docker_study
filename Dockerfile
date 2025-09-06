49b64425f92f2d17be2dcf32301ce0719024d476
ergrassa <ergrassa@yandex.ru>
2025-09-06 17:14:33 +0300

    webapp

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM python:3.13-slim
 
 WORKDIR /app
-COPY main.py .
+COPY requirements.txt .
+RUN pip install -r requirements.txt
 
-CMD ["python", "main.py"]
+COPY main.py .
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 import os
 import random
+from fastapi import FastAPI, Query
 
-min = os.getenv('MIN_VALUE')
-max = os.getenv('MAX_VALUE')
-
-
-def give_a_num(min_input, max_input):
-  try:
-    return random.randint(int(min_input), int(max_input))
-  except Exception as e:
-    print(e)
+app = FastAPI()
+mult = os.environ.get('MULTIPLIER', 1)
 
 
-if __name__ == '__main__':
-  print(min, max)
-  print(give_a_num(min, max))
+"""
+Generates and returns a random number within a specified range.
+
+This endpoint accepts optional query parameters 'min' and 'max' to define the range for the random number generation.
+If not provided, 'min' defaults to 1 and 'max' defaults to 100. Both parameters must be greater than or equal to 1.
+
+Args:
+    min (int, optional): The minimum value for the random number (inclusive). Defaults to 1. Must be >= 1.
+    max (int, optional): The maximum value for the random number (inclusive). Defaults to 100. Must be >= 1.
+
+Returns:
+    dict: A dictionary containing the min, max, generated random number, and a descriptive message.
+"""
+@app.get('/')
+def give_a_num(min: int = Query(1, ge=1), max: int = Query(100, ge=1)):
+  rand = random.randint(int(min), int(max)) * int(mult)
+  return {
+    'min': min,
+    'max': max,
+    'mult': mult,
+    'rand': rand,
+    'message': f"Here is a random number {rand} between {min} and {max}, multiplied by {mult}"
+  }
